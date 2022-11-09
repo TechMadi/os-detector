f1d178399a581b86acc5e3c34f0d8fd4942981b0
Tech Madi <zolasmandela4@gmail.com>
2022-11-09 13:38:55 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BrowserDetectorService } from './service/browser-detector.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'browser-detect';
+  browserName: string = '';
+  openLink = 'https://www.instagram.com/';
+  url =
+    'intent://instagram.com/#Intent;scheme=https;package=com.instagram.android;end';
+  constructor(private browserService: BrowserDetectorService) {}
+
+  ngOnInit() {
+    this.getBrowser();
+  }
+
+  getBrowser() {
+    this.browserName = this.browserService.getBrowserName();
+
+    this.browserService.isMobile() ? (this.openLink = this.url) : this.openLink;
+  }
 }
